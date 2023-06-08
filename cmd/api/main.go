899821a2cd51ba54cdd268d9824866eafa0e2cec
Oleksandr Andriienko <oandriie@redhat.com>
2023-06-08 18:57:34 +0300

    Hmmm. temp...
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,8 +17,8 @@ limitations under the License.
 package main
 
 import (
-	"context"
-	"fmt"
+	// "context"
+	// "fmt"
 	// "net"
 	"net/http"
 	"path"
@@ -30,28 +30,28 @@ import (
 	"go.uber.org/zap"
 	// "go.uber.org/zap/zapcore"
 
-	"github.com/golang-jwt/jwt/v4"
+	// "github.com/golang-jwt/jwt/v4"
 	// grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	// grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	// grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	// "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	// grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	// prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	// "github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/tektoncd/results/pkg/api/server/config"
 	"github.com/tektoncd/results/pkg/api/server/logger"
-	v1alpha2 "github.com/tektoncd/results/pkg/api/server/v1alpha2"
+	// v1alpha2 "github.com/tektoncd/results/pkg/api/server/v1alpha2"
 	// "github.com/tektoncd/results/pkg/api/server/v1alpha2/auth"
-	v1alpha2pb "github.com/tektoncd/results/proto/v1alpha2/results_go_proto"
+	// v1alpha2pb "github.com/tektoncd/results/proto/v1alpha2/results_go_proto"
 	_ "go.uber.org/automaxprocs"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/credentials/insecure"
+	// "google.golang.org/grpc"
+	// "google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/credentials/insecure"
 	// "google.golang.org/grpc/health"
 	// healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	// "google.golang.org/grpc/reflection"
-	"gorm.io/driver/postgres"
+	// "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
 	"log"
@@ -70,56 +70,60 @@ func main() {
 	// Connect to the database.
 	// DSN derived from https://pkg.go.dev/gorm.io/driver/postgres
 
-	dbURI := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", serverConfig.DB_HOST, serverConfig.DB_USER, serverConfig.DB_PASSWORD, serverConfig.DB_NAME, serverConfig.DB_PORT, serverConfig.DB_SSLMODE)
+	// dbURI := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", serverConfig.DB_HOST, serverConfig.DB_USER, serverConfig.DB_PASSWORD, serverConfig.DB_NAME, serverConfig.DB_PORT, serverConfig.DB_SSLMODE)
 
 	gormConfig := &gorm.Config{}
 	if log.Level() != zap.DebugLevel {
 		gormConfig.Logger = gormlogger.Default.LogMode(gormlogger.Silent)
 	}
-	db, err := gorm.Open(postgres.Open(dbURI), gormConfig)
-	if err != nil {
-		log.Fatalf("Failed to open the results.db: %v", err)
-	}
+	// db, err := gorm.Open(postgres.Open(dbURI), gormConfig)
+	// if err != nil {
+	// 	log.Fatalf("Failed to open the results.db: %v", err)
+	// }
 
 	// Load TLS cert for server
-	creds, tlsError := credentials.NewServerTLSFromFile(path.Join(serverConfig.TLS_PATH, "tls.crt"), path.Join(serverConfig.TLS_PATH, "tls.key"))
-	if tlsError != nil {
-		log.Infof("Error loading TLS key pair for server: %v", tlsError)
-		log.Warn("Creating server without TLS")
-		creds = insecure.NewCredentials()
-	}
+	// creds, tlsError := credentials.NewServerTLSFromFile(path.Join(serverConfig.TLS_PATH, "tls.crt"), path.Join(serverConfig.TLS_PATH, "tls.key"))
+	// if tlsError != nil {
+	// 	log.Infof("Error loading TLS key pair for server: %v", tlsError)
+	// 	log.Warn("Creating server without TLS")
+	// 	creds = insecure.NewCredentials()
+	// }
 
 	// Register API server(s)
-	v1a2, err := v1alpha2.New(serverConfig, log, db)
-	if err != nil {
-		log.Fatalf("Failed to create server: %v", err)
-	}
+	// v1a2, err := v1alpha2.New(serverConfig, log, db)
+	// if err != nil {
+	// 	log.Fatalf("Failed to create server: %v", err)
+	// }
 
-	s := grpc.NewServer(
-		grpc.Creds(creds),
-	)
-	v1alpha2pb.RegisterResultsServer(s, v1a2)
+	// s := grpc.NewServer(
+	// 	grpc.Creds(creds),
+	// )
+	// v1alpha2pb.RegisterResultsServer(s, v1a2)
 
 	// Load REST client TLS cert to connect to the gRPC server
-	if tlsError == nil {
-		creds, err = credentials.NewClientTLSFromFile(path.Join(serverConfig.TLS_PATH, "tls.crt"), serverConfig.TLS_HOSTNAME_OVERRIDE)
-		if err != nil {
-			log.Fatalf("Error loading TLS certificate for REST: %v", err)
-		}
-	}
+	// if tlsError == nil {
+	// 	creds, err = credentials.NewClientTLSFromFile(path.Join(serverConfig.TLS_PATH, "tls.crt"), serverConfig.TLS_HOSTNAME_OVERRIDE)
+	// 	if err != nil {
+	// 		log.Fatalf("Error loading TLS certificate for REST: %v", err)
+	// 	}
+	// }
 
 	// Register gRPC server endpoint for gRPC gateway
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	// ctx := context.Background()
+	// ctx, cancel := context.WithCancel(ctx)
+	// defer cancel()
 	mux := runtime.NewServeMux()
+	mux.Handle("/", runtime.Pattern{}, func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+		log.Info("request....")
+		w.Write([]byte("Hello"))
+	})
 
 	// Start REST proxy server
 	log.Infof("REST server Listening on: %s", serverConfig.REST_PORT)
-	if tlsError != nil {
-		log.Fatal(http.ListenAndServe(":"+serverConfig.REST_PORT, mux))
-	} else {
+	// if tlsError != nil {
+	// 	log.Fatal(http.ListenAndServe(":"+serverConfig.REST_PORT, mux))
+	// } else {
 		log.Fatal(http.ListenAndServeTLS(":"+serverConfig.REST_PORT, path.Join(serverConfig.TLS_PATH, "tls.crt"), path.Join(serverConfig.TLS_PATH, "tls.key"), mux))
-	}
+	// }
 
 }
